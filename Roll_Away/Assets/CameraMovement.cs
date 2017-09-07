64891fbaf92fc602b4bb36c9b0e5647aa6a963ad
Rachana040 <rachanasoma99@gmail.com>
2017-09-07 17:46:09 +0530

    Roll_Away
    A simple ball collecting game made in unity

---
+++ Roll_Away/Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject Player;
+    private Vector3 offset;
+	void Start () {
+        offset = transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Roll_Away/Assets/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject enemy;
+    int start;
+    public float speed;
+    private int count;
+    public Text countText;
+   
+	// Use this for initialization
+	void Start () {
+
+
+        start = 0;
+        count = 0;
+        setCountText();
+        
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "item")
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+         
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+            countText.text = "GAME OVER";
+           
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (start == 0)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+    void setCountText()
+    {
+        if(count!=4)
+            countText.text = "Count: " + count.ToString();
+        else
+        {
+            countText.text = "BRAVO!!";
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (count == 4)
+        {
+            start = 1;
+        }
+	}
+}
